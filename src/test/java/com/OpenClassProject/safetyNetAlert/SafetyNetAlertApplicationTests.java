2a4eaae8bd7a6aff5626c6530cbf89c4836fbb2e
Fagotto <joffrey.fagotto4@laposte.net>
2022-02-09 13:59:02 +0100

    initial commit : projet spring avec spring initializr

---
+++ src/test/java/com/OpenClassProject/safetyNetAlert/SafetyNetAlertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OpenClassProject.safetyNetAlert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetyNetAlertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
